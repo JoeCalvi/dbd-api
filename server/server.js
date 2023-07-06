66cac6bb1925da11c7d535820b06891589d808a1
JoeCalvi <joecalvi@me.com>
2023-07-06 17:39:08 -0600

    .

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Perk = require('./models/Perk'),
+    bodyParser = require('body-parser');
+
+
 
 app.listen(port);
 
