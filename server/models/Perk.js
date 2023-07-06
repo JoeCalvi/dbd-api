7db979091794157c59eac5c7a5789e7cc4e6e082
JoeCalvi <joecalvi@me.com>
2023-07-06 15:55:59 -0600

    starting

---
+++ server/models/Perk.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PerkSchema = new Schema({
+    role: { type: String, required: true },
+    name: { type: String, required: true },
+    belongs_to: { type: String, required: true, default: "N/A" },
+    belongs_to_id: { type: Schema.Types.ObjectId, ref: "Character" },
+    icon: { type: String, required: true },
+    description: { type: String, required: true }
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+PerkSchema.virtual('character', {
+    localField: 'belongs_to_id',
+    foreignField: '_id',
+    justOne: true,
+    ref: 'Survivor' || 'Killer'
+})
