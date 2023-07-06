ee7a3705ad643aa79d5826ba75ca16e17492045a
JoeCalvi <joecalvi@me.com>
2023-07-06 17:28:43 -0600

    route functions

---
+++ server/controllers/PerksController.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose'),
+    Perk = mongoose.model('Perks');
+
+exports.getAllPerks = function (req, res) {
+    Perk.find({}, function (err, perk) {
+        if (err) {
+            return res.send(err);
+        } else {
+            return res.json(perk);
+        };
+    });
+};
+
+exports.addPerk = function (req, res) {
+    const perk = new Perk(req.body);
+    perk.save(function (err, perk) {
+        if (err) {
+            return res.send(err);
+        } else {
+            return res.json(perk);
+        };
+    });
+};
+
+exports.getPerkById = function (req, res) {
+    Perk.findById(req.params.perkId, function (err, perk) {
+        if (err) {
+            return res.send(err);
+        } else {
+            return res.json(perk);
+        };
+    });
+};
+
+exports.updatePerk = function (res, res) {
+    Perk.findByIdAndUpdate(req.params.perkId, req.body, { new: true }, function (err, perk) {
+        if (err) {
+            return res.send(err);
+        } else {
+            return res.json(perk);
+        };
+    });
+};
+
