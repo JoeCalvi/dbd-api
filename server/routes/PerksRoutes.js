5968b048119e3203654531a77c3747bbfa1afd17
JoeCalvi <joecalvi@me.com>
2023-07-10 16:11:58 -0600

    .

--- server/routes/PerksRoutes.js
+++ server/routes/PerksRoutes.js
@@ -1,6 +1,9 @@
 module.exports = function (app) {
     const perksController = require('../controllers/PerksController');
 
+    app.route('/api/')
+        .get(perksController.getCharacterByName);
+
     app.route('/api/perks')
         .get(perksController.getAllPerks)
         .post(perksController.addGenericPerk);
@@ -17,9 +20,6 @@ module.exports = function (app) {
     app.route('/api/:characterId/perks')
         .post(perksController.addPerk)
         .get(perksController.getPerksByCharacterId);
-
-    app.route('/api/')
-        .get(perksController.getCharacterByName);
 
     app.route('/api/perks/:perkId')
         .get(perksController.getPerkById)
