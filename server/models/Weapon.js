6837ab9391cf67272c03f97700d422fc49ef232e
JoeCalvi <joecalvi@me.com>
2023-07-19 16:23:40 -0600

    updated weapon model

--- server/models/Weapon.js
+++ server/models/Weapon.js
@@ -5,11 +5,11 @@ const WeaponSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
-    killerId: { type: Schema.Types.ObjectId, ref: 'Killers', required: true }
+    killer_id: { type: Schema.Types.ObjectId, ref: 'Killers', required: true },
 }, { timestamps: true, toJSON: { virtuals: true } })
 
 WeaponSchema.virtual('killer', {
-    localField: 'killerId',
+    localField: 'killer_id',
     foreignField: '_id',
     justOne: true,
     ref: 'Killers'
