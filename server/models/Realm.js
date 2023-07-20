b7ae741ae5754cfe2692aef563c305247cbb4121
JoeCalvi <joecalvi@me.com>
2023-07-20 11:12:41 -0600

    realm model should be good

--- server/models/Realm.js
+++ server/models/Realm.js
@@ -5,20 +5,20 @@ const RealmSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     location: { type: String, required: true },
-    associated_killers_id: { type: [Schema.Types.ObjectId], ref: 'Killers' },
-    maps_id: { type: [Schema.Types.ObjectId], ref: 'Maps' },
+    associated_killers_arr: { type: [Schema.Types.ObjectId], ref: 'Killers' },
+    maps_arr: { type: [Schema.Types.ObjectId], ref: 'Maps' },
     // chapter_id: { type: Schema.Types.ObjectId, ref: 'Chapters'}
 }, { timestamps: true, toJSON: { virtuals: true } });
 
 RealmSchema.virtual('associated_killers', {
-    localField: 'associated_killers',
+    localField: 'associated_killers_arr',
     foreignField: '_id',
     justOne: true,
     ref: 'Killers'
 });
 
 RealmSchema.virtual('maps', {
-    localField: 'maps',
+    localField: 'maps_arr',
     foreignField: '_id',
     justOne: true,
     ref: 'Maps'
