cb5759ddf53874435535c67d338f53b927cfcd20
JoeCalvi <joecalvi@me.com>
2023-07-21 12:33:32 -0600

    getPowerByKillerName

--- server/controllers/PowersController.js
+++ server/controllers/PowersController.js
@@ -1,6 +1,6 @@
-// const mongoose = require('mongoose');
-// const Power = mongoose.model('Powers');
-// const Killer = mongoose.model('Killers');
+const mongoose = require('mongoose');
+const Power = mongoose.model('Powers');
+const Killer = mongoose.model('Killers');
 
 exports.getAllPowers = async function (req, res) {
     try {
@@ -23,26 +23,24 @@ exports.addPower = async function (req, res) {
     }
 };
 
-// exports.getMapById = async function (req, res) {
-//     try {
-//         const map = await Map.findById(req.params.mapId)
-//             .populate('realm');
-//         return res.json(map);
-//     } catch (err) {
-//         res.send(err);
-//     }
-// };
+exports.getPowerById = async function (req, res) {
+    try {
+        const power = await Power.findById(req.params.powerId)
+            .populate('killer');
+        return res.json(power);
+    } catch (err) {
+        res.send(err);
+    }
+};
 
-// // TODO this returns an empty object
-// exports.getMapsByRealmName = async function (req, res) {
+// exports.getPowerByKillerName = async function (req, res) {
 //     try {
 //         const query = req.query;
-//         const name = query.realm_name.replace('-', ' ').toLowerCase();
-//         const realms = await Realm.find()
-//             .populate('maps');
-//         const realm = realms.find(r => r.name.toLowerCase() == name);
-//         const maps = realm.maps
-//         return res.send(maps);
+//         const name = query.killer_name.replace('-', ' ').toLowerCase();
+//         const killers = await Killer.find()
+//         const killer = killers.find(k => k.killer_name.toLowerCase() == name);
+//         const power = await Power.findById(killer.power_id)
+//         return res.send(power);
 //     } catch (err) {
 //         res.send(err)
 //     }
