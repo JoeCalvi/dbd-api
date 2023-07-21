a887731b0a50529a7bf2518fbe5c3e261cfe14f0
JoeCalvi <joecalvi@me.com>
2023-07-21 12:46:06 -0600

    .

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const express = require('express'),
     Realm = require('./models/Realm'),
     Map = require('./models/Map'),
     Power = require('./models/Power'),
+    StatusEffect = require('./models/StatusEffect'),
     bodyParser = require('body-parser');
 
 const httpServer = createServer(app);
