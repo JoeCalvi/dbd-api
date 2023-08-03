d2dd3b8bcde0e2cce9ff23b79acdae9d4abde319
JoeCalvi <joecalvi@me.com>
2023-08-03 10:52:32 -0600

    added applies_to enum on SE model

--- server/models/StatusEffect.js
+++ server/models/StatusEffect.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const StatusEffectSchema = new Schema({
     name: { type: String, required: true },
     type: { type: String, enum: ['Buff', 'Debuff'], required: true },
+    applies_to: { type: String, enum: ['Killer', 'Survivor', 'Both'] },
     description: { type: String, required: true },
     icon: { type: String, required: true }
 });
