83c2387312a77faace144b6177751e27f10865af
JoeCalvi <joecalvi@me.com>
2023-08-04 15:02:00 -0600

    .

--- server/routes/PerksRoutes.js
+++ server/routes/PerksRoutes.js
@@ -8,21 +8,27 @@ module.exports = function (app) {
     app.route('/perks/query')
         .get((req, res) => {
             if (req.query.perk_name) {
+
                 // ?perk_name=perkName 
                 // if perk name is more than one word, separate with hyphens
                 // will return specific perk
                 return perksController.getPerkByName(req, res);
+
             } else if (req.query.type) {
+
                 // ?type=perk_type
                 // supports 'hex', 'boon', 'scourge', and 'teamwork'
                 // returns all perks of that type
                 return perksController.getPerksByType(req, res);
+
             } else if (req.query.character_name) {
+
                 // ?character_name=killer_name OR ?character_name=name
                 // simple name query in most cases, but there are one-offs
                 // ex: ?character_name=ashley-williams will return nothing
-                // ?character_name=ashley-joanna-'ash'-williams returns ash's perks
+                // ?character_name=ashley-joanna-'ash'-williams returns perks
                 return perksController.getPerksByCharacterName(req, res);
+
             }
         })
 
