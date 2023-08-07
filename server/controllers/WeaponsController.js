3b3c97929922f8abd3d893c28039cf8439606a82
JoeCalvi <joecalvi@me.com>
2023-08-07 11:32:54 -0600

    weaponsController code cleanup

--- server/controllers/WeaponsController.js
+++ server/controllers/WeaponsController.js
@@ -6,9 +6,10 @@ exports.getAllWeapons = async function (req, res) {
     try {
         const weapons = await Weapon.find({})
             .populate('killer', 'killer_name portrait');
-        res.json(weapons);
-    } catch (err) {
-        res.send(err);
+
+        return res.json(weapons);
+    } catch (error) {
+        res.send(error);
     }
 };
 
@@ -16,50 +17,58 @@ exports.addWeapon = async function (req, res) {
     try {
         const weapon = new Weapon(req.body);
         const savedWeapon = await weapon.save();
-        res.json(savedWeapon);
-    } catch (err) {
-        res.send(err);
+
+        return res.json(savedWeapon);
+    } catch (error) {
+        res.send(error);
     }
 };
 
 exports.getWeaponById = async function (req, res) {
     try {
         const weapon = await Weapon.findById(req.params.weaponId)
-            .populate('killer');
-        res.json(weapon);
-    } catch (err) {
-        res.send(err);
+            .populate({ path: 'killer', select: 'killer_name portrait overview chapter_id',
+            populate: { path: 'chapter', select: 'name number release_date image' }});
+
+        return res.json(weapon);
+    } catch (error) {
+        res.send(error);
     }
 };
 
 exports.getWeaponByKillerName = async function (req, res) {
     try {
         const query = req.query;
-        const name = query.killer_name.replace('-', ' ').toLowerCase();
+        const name = query.killer_name.replaceAll('-', ' ').toLowerCase();
         const killers = await Killer.find()
         const killer = killers.find(k => k.killer_name.toLowerCase() == name);
         const weapon_id = killer.weapon_id
         const weapon = await Weapon.findById(weapon_id)
+            .populate({ path: 'killer', select: 'killer_name portrait overview chapter_id',
+            populate: { path: 'chapter', select: 'name number release_date image' }});
+
         return res.json(weapon);
-    } catch (err) {
-        res.send(err)
+    } catch (error) {
+        res.send(error);
     }
-}
+};
 
 exports.updateWeapon = async function (req, res) {
     try {
         const weapon = await Weapon.findByIdAndUpdate(req.params.weaponId, req.body, { new: true });
-        res.json(weapon);
-    } catch (err) {
-        res.send(err);
+
+        return res.json(weapon);
+    } catch (error) {
+        res.send(error);
     }
 };
 
 exports.deleteWeapon = async function (req, res) {
     try {
         const weapon = await Weapon.findByIdAndDelete(req.params.weaponId);
-        res.json({ message: 'Weapon deleted.' });
-    } catch (err) {
-        res.send(err);
+
+        return res.json({ message: 'Weapon deleted.' });
+    } catch (error) {
+        res.send(error);
     }
 };
