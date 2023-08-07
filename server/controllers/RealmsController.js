b417d41e5432e713d5469574c113ee03a326bb9f
JoeCalvi <joecalvi@me.com>
2023-08-07 12:25:29 -0600

    realmsController code cleanup

--- server/controllers/RealmsController.js
+++ server/controllers/RealmsController.js
@@ -8,9 +8,10 @@ exports.getAllRealms = async function (req, res) {
             .populate('associated_killers', 'killer_name portrait')
             .populate('associated_chapters', 'name number release_date image')
             .populate('maps', 'name image layout');
+
         return res.json(realms);
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
 
@@ -18,9 +19,10 @@ exports.addRealm = async function (req, res) {
     try {
         const realm = new Realm(req.body);
         const savedRealm = await realm.save();
+
         return res.json(savedRealm);
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
 
@@ -28,41 +30,49 @@ exports.getRealmById = async function (req, res) {
     try {
         const realm = await Realm.findById(req.params.realmId)
             .populate('associated_killers', 'killer_name portrait')
-            .populate('maps');
+            .populate('associated_chapters', 'name number release_date image')
+            .populate('maps', 'name description image layout');
+
         return res.json(realm);
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
 
 exports.getRealmByKillerName = async function (req, res) {
     try {
         const query = req.query;
-        const name = query.killer_name.replace('-', ' ').toLowerCase();
+        const name = query.killer_name.replaceAll('-', ' ').toLowerCase();
         const killers = await Killer.find()
         const killer = killers.find(k => k.killer_name.toLowerCase() == name);
         const realm_id = killer.realm_id
         const realm = await Realm.findById(realm_id)
+            .populate('associated_killers', 'killer_name portrait')
+            .populate('associated_chapters', 'name number release_date image')
+            .populate('maps', 'name description image layout');
+
         return res.json(realm);
-    } catch (err) {
-        res.send(err)
+    } catch (error) {
+        res.send(error);
     }
-}
+};
 
 exports.updateRealm = async function (req, res) {
     try {
         const realm = await Realm.findByIdAndUpdate(req.params.realmId, req.body, { new: true });
+
         return res.json(realm);
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
 
 exports.deleteRealm = async function (req, res) {
     try {
         const realm = await Realm.findByIdAndDelete(req.params.realmId);
+
         return res.json({ message: 'Realm deleted.' });
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
