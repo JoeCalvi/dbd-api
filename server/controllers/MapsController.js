37c226cc08961146cf946ff858294e041907850b
JoeCalvi <joecalvi@me.com>
2023-08-07 12:55:22 -0600

    mapsController code cleanup

--- server/controllers/MapsController.js
+++ server/controllers/MapsController.js
@@ -5,77 +5,83 @@ const Map = mongoose.model('Maps');
 exports.getAllMapsInARealm = async function (req, res) {
     try {
         const realm = await Realm.findById(req.params.realmId)
-            .populate('maps')
+            .populate('maps', 'name description image layout');
         const maps = realm.maps;
+
         return res.send(maps);
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
 
 exports.getAllMaps = async function (req, res) {
     try {
         const maps = await Map.find({})
-            .populate('realm', 'name location');
+            .populate('realm', 'name location image');
+
         return res.json(maps);
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
 
 exports.addMap = async function (req, res) {
     try {
         const map = new Map(req.body);
-        map.realm_id = req.params.realmId
+        map.realm_id = req.params.realmId;
         const savedMap = await map.save();
         const realm = await Realm.findById(req.params.realmId);
-        realm.maps.push(savedMap._id)
+        realm.maps.push(savedMap._id);
         await realm.save();
+
         return res.json(savedMap);
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
 
 exports.getMapById = async function (req, res) {
     try {
         const map = await Map.findById(req.params.mapId)
-            .populate('realm');
+            .populate('realm', 'name location image');
+
         return res.json(map);
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
 
-// TODO this returns an empty object
 exports.getMapsByRealmName = async function (req, res) {
     try {
         const query = req.query;
-        const name = query.realm_name.replace('-', ' ').toLowerCase();
+        const name = query.realm_name.replaceAll('-', ' ').toLowerCase();
         const realms = await Realm.find()
-            .populate('maps');
+            .populate('maps', 'name description image layout');
         const realm = realms.find(r => r.name.toLowerCase() == name);
         const maps = realm.maps
+
         return res.send(maps);
-    } catch (err) {
-        res.send(err)
+    } catch (error) {
+        res.send(error)
     }
 }
 
 exports.updateMap = async function (req, res) {
     try {
         const map = await Map.findByIdAndUpdate(req.params.mapId, req.body, { new: true });
+
         return res.json(map);
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
 
 exports.deleteMap = async function (req, res) {
     try {
         const map = await Map.findByIdAndDelete(req.params.mapId);
+
         return res.json({ message: 'Map deleted.' });
-    } catch (err) {
-        res.send(err);
+    } catch (error) {
+        res.send(error);
     }
 };
