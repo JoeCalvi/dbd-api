4436cb8c6f47ab21ff23a95d34e0da66f97babfb
JoeCalvi <joecalvi@me.com>
2023-08-07 13:34:12 -0600

    added chapter_id to map model

--- server/models/Map.js
+++ server/models/Map.js
@@ -6,7 +6,8 @@ const MapSchema = new Schema({
     description: { type: String, required: true },
     image: { type: String, required: true },
     layout: { type: String, required: true },
-    realm_id: { type: Schema.Types.ObjectId, ref: 'Realms' }
+    realm_id: { type: Schema.Types.ObjectId, ref: 'Realms' },
+    chapter_id: { type: Schema.Types.ObjectId, ref: 'Chapters' }
 }, { timestamps: true, toJSON: { virtuals: true } });
 
 MapSchema.virtual('realm', {
@@ -15,5 +16,13 @@ MapSchema.virtual('realm', {
     justOne: true,
     ref: 'Realms'
 });
+
+MapSchema.virtual('chapter', {
+    localField: 'chapter_id',
+    foreignField: '_id',
+    justOne: true,
+    ref: 'Chapters'
+});
+
 
 module.exports = mongoose.model('Maps', MapSchema);
